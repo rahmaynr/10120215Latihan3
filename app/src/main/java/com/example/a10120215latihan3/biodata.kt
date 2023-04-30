790334014c6dd4f704dc4310c82c38a5d7a6e85c
rahmaynr <rahmaa55221@gmail.com>
2023-04-30 10:14:58 +0700

    New

--- app/src/main/java/com/example/a10120215latihan3/biodata.kt
+++ app/src/main/java/com/example/a10120215latihan3/biodata.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class biodata : AppCompatActivity() {
     private lateinit var btn_selanjutnya: Button
@@ -13,7 +14,9 @@ class biodata : AppCompatActivity() {
 
         btn_selanjutnya = findViewById(R.id.btn_selanjutnya)
         btn_selanjutnya.setOnClickListener {
+            val biodata = findViewById<EditText>(R.id.editText).text.toString()
             val intent = Intent(this, say::class.java)
+            intent.putExtra("editText",biodata)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/a10120215latihan3/say.kt
+++ app/src/main/java/com/example/a10120215latihan3/say.kt
@@ -2,10 +2,21 @@ package com.example.a10120215latihan3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class say : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_say)
+
+        val biodata = intent.getStringExtra("editText")
+        val tampil = "Beres! Sekarang kamu\nudah bisa ngecek penggunaan HP mu\ntiap hari buat bantu kamu\nngatur waktu :)"
+        Log.d("debug",tampil)
+        val searchText = "kamu"
+        val containsSearchText = tampil.contains(searchText)
+        val replaceText = tampil.replace(searchText, biodata.toString(),false)
+        val say = findViewById<TextView>(R.id.textView11)
+        say.text = replaceText
     }
 }
